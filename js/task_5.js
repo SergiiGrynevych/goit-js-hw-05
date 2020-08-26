00a81797d1d22b8b9f4a7ac2fa5ba260e6ffddb8
SergiiGrynevych <grinevich.sergey.official@gmail.com>
2020-08-26 20:12:02 +0300

    решена 5 задача

--- js/task_5.js
+++ js/task_5.js
@@ -1,16 +1,41 @@
 class Car {
-    // Write code under this line
-    getSpecs(car) {
-        return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car._price}`;
+  // Write code under this line
+  static getSpecs(car) {
+    return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car._price}`;
+  }
+  constructor(object, speed = 0, isOn = false, distance = 0) {
+    this.speed = speed;
+    this._price = object.price;
+    this.maxSpeed = object.maxSpeed;
+    this.isOn = isOn;
+    this.distance = distance;
+  }
+  get price() {
+    return this._price;
+  }
+  set price(value) {
+    this._price = value;
+  }
+  turnOn() {
+    this.isOn = true;
+  }
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  accelerate(value) {
+    this.speed + value <= this.maxSpeed
+      ? (this.speed += value)
+      : (this.speed = this.maxSpeed);
+  }
+  decelerate(value) {
+    this.speed - value > 0 ? (this.speed -= value) : (this.speed = 0);
+  }
+  drive(hours) {
+    if (this.isOn) {
+      this.distance += hours * this.speed;
     }
-    constructor() { }
-    get price() { }
-    set price(value) { }
-    turnOn() { }
-    turnOff() { }
-    accelerate(value) { }
-    decelerate(value) { }
-    drive(hours) { }
+  }
 }
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
@@ -18,7 +43,7 @@ mustang.turnOn();
 mustang.accelerate(50);
 mustang.drive(2);
 
-console.log(Car.getSpecs(mustang));
+// console.log(Car.getSpecs(mustang));
 // 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
 
 mustang.decelerate(20);
@@ -31,8 +56,6 @@ mustang.turnOff();
 // console.log(mustang.price); // 2000
 mustang.price = 4000;
 // console.log(mustang.price); // 4000
-
-
 
 // Задача 5 - 5
 // класс
